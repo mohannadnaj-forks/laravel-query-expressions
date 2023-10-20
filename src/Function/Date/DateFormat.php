bd1c5bebf32f18d19324c07b5cdc87dc2092e460
Mohannad Najjar <mohannadnaj@me.com>
2023-10-20 09:44:57 +0300

    `composer format`

--- src/Function/Date/DateFormat.php
+++ src/Function/Date/DateFormat.php
@@ -47,88 +47,88 @@ class DateFormat implements Expression
     protected function getDateFormatChar(Grammar $grammar, string $char): string
     {
         return match ($this->identify($grammar)) {
-                'mysql' => match ($char) {
-                    'Y' => '%Y',
-                    'y' => '%y',
-                    'M' => '%b',
-                    'm' => '%m',
-                    'n' => '%c',
-                    'F' => '%M',
-                    'D' => '%a',
-                    'd' => '%d',
-                    'l' => '%W',
-                    'j' => '%e',
-                    'W' => '%v',
-                    'H' => '%H',
-                    'h' => '%h',
-                    'i' => '%i',
-                    's' => '%s',
-                    'A' => '%p',
-                    default => $char,
-                },
-                'sqlite' => match ($char) {
-                    'Y' => '%Y',
-                    // No format for 2 digit year number.
-                    'y' => '%Y',
-                    // No format for 3 letter month name.
-                    'M' => '%m',
-                    'm' => '%m',
-                    // No format for month number without leading zeros.
-                    'n' => '%m',
-                    // No format for full month name.
-                    'F' => '%m',
-                    // No format for 3 letter day name.
-                    'D' => '%d',
-                    'd' => '%d',
-                    // No format for full day name.
-                    'l' => '%d',
-                    // no format for day of month number without leading zeros.
-                    'j' => '%d',
-                    'W' => '%W',
-                    'H' => '%H',
-                    // No format for 12 hour with leading zeros.
-                    'h' => '%H',
-                    'i' => '%M',
-                    's' => '%S',
-                    // No format for AM/PM.
-                    // 'A' => ''.
-                    default => $char,
-                },
-                'pgsql' => match ($char) {
-                    'Y' => 'YYYY',
-                    'y' => 'YY',
-                    'M' => 'Mon',
-                    'm' => 'MM',
-                    // No format for Numeric representation of a month, without leading zeros.
-                    'n' => 'MM',
-                    'F' => 'Month',
-                    'D' => 'Dy',
-                    'd' => 'DD',
-                    'l' => 'Day',
-                    // No format for Day of the month without leading zeros.
-                    'j' => 'DD',
-                    'W' => 'IW',
-                    'H' => 'HH24',
-                    'h' => 'HH12',
-                    'i' => 'MI',
-                    's' => 'SS',
-                    'A' => 'AM',
-                    default => $char,
-                },
-                'sqlsrv' => match ($char) {
-                    'A' => 'tt',
-                    'd' => 'dd',
-                    'F' => 'MMM',
-                    'h' => 'hh',
-                    'H' => 'HH',
-                    'i' => 'mm',
-                    'm' => 'MM',
-                    // No format for 3 letter month name.
-                    'M' => 'MMM',
-                    's' => 'ss',
-                    'Y' => 'yyyy',
-                    default => $char,
-                },
-            };
+            'mysql' => match ($char) {
+                'Y' => '%Y',
+                'y' => '%y',
+                'M' => '%b',
+                'm' => '%m',
+                'n' => '%c',
+                'F' => '%M',
+                'D' => '%a',
+                'd' => '%d',
+                'l' => '%W',
+                'j' => '%e',
+                'W' => '%v',
+                'H' => '%H',
+                'h' => '%h',
+                'i' => '%i',
+                's' => '%s',
+                'A' => '%p',
+                default => $char,
+            },
+            'sqlite' => match ($char) {
+                'Y' => '%Y',
+                // No format for 2 digit year number.
+                'y' => '%Y',
+                // No format for 3 letter month name.
+                'M' => '%m',
+                'm' => '%m',
+                // No format for month number without leading zeros.
+                'n' => '%m',
+                // No format for full month name.
+                'F' => '%m',
+                // No format for 3 letter day name.
+                'D' => '%d',
+                'd' => '%d',
+                // No format for full day name.
+                'l' => '%d',
+                // no format for day of month number without leading zeros.
+                'j' => '%d',
+                'W' => '%W',
+                'H' => '%H',
+                // No format for 12 hour with leading zeros.
+                'h' => '%H',
+                'i' => '%M',
+                's' => '%S',
+                // No format for AM/PM.
+                // 'A' => ''.
+                default => $char,
+            },
+            'pgsql' => match ($char) {
+                'Y' => 'YYYY',
+                'y' => 'YY',
+                'M' => 'Mon',
+                'm' => 'MM',
+                // No format for Numeric representation of a month, without leading zeros.
+                'n' => 'MM',
+                'F' => 'Month',
+                'D' => 'Dy',
+                'd' => 'DD',
+                'l' => 'Day',
+                // No format for Day of the month without leading zeros.
+                'j' => 'DD',
+                'W' => 'IW',
+                'H' => 'HH24',
+                'h' => 'HH12',
+                'i' => 'MI',
+                's' => 'SS',
+                'A' => 'AM',
+                default => $char,
+            },
+            'sqlsrv' => match ($char) {
+                'A' => 'tt',
+                'd' => 'dd',
+                'F' => 'MMM',
+                'h' => 'hh',
+                'H' => 'HH',
+                'i' => 'mm',
+                'm' => 'MM',
+                // No format for 3 letter month name.
+                'M' => 'MMM',
+                's' => 'ss',
+                'Y' => 'yyyy',
+                default => $char,
+            },
+        };
     }
 }
