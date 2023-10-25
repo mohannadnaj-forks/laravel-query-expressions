95853628f5c9fb73f234a0447bb4403f8249822e
Mohannad Najjar <mohannadnaj@me.com>
2023-10-25 03:41:17 +0300

    wip

--- src/Function/Date/DateFormat.php
+++ src/Function/Date/DateFormat.php
@@ -6,13 +6,117 @@ namespace Tpetry\QueryExpressions\Function\Date;
 
 use Illuminate\Contracts\Database\Query\Expression;
 use Illuminate\Database\Grammar;
+use Illuminate\Database\Query\Expression as QueryExpression;
 use Tpetry\QueryExpressions\Concerns\IdentifiesDriver;
 use Tpetry\QueryExpressions\Concerns\StringizeExpression;
+use Tpetry\QueryExpressions\Function\String\Concat;
 
 class DateFormat implements Expression
 {
     use IdentifiesDriver;
     use StringizeExpression;
+
+    /**
+     * @var array<'mysql'|'sqlite'|'pgsql'|'sqlsrv', array<string, string>>
+     */
+    protected array $formatCharacters = [
+        'mysql' => [
+            'Y' => '%Y',
+            'y' => '%y',
+            'o' => '%x',
+            'M' => '%b',
+            'm' => '%m',
+            'n' => '%c',
+            'F' => '%M',
+            'D' => '%a',
+            'd' => '%d',
+            'l' => '%W',
+            'j' => '%e',
+            'W' => '%v',
+            'H' => '%H',
+            'h' => '%h',
+            'i' => '%i',
+            's' => '%s',
+            'A' => '%p',
+        ],
+        'sqlite' => [
+            'Y' => '%Y',
+            // No format for 3 letter month name.
+            'M' => '%m',
+            'm' => '%m',
+            // No format for month number without leading zeros.
+            'n' => '%m',
+            // No format for full month name.
+            'F' => '%m',
+            // No format for 3 letter day name.
+            'D' => '%d',
+            'd' => '%d',
+            // No format for full day name.
+            'l' => '%d',
+            // no format for day of month number without leading zeros.
+            'j' => '%d',
+            'W' => '%W',
+            'H' => '%H',
+            // No format for 12 hour with leading zeros.
+            'h' => '%H',
+            'i' => '%M',
+            's' => '%S',
+            // No format for AM/PM.
+            // 'A' => ''.
+        ],
+        'pgsql' => [
+            'Y' => 'YYYY',
+            'y' => 'YY',
+            'M' => 'Mon',
+            'n' => 'FMMM',
+            'm' => 'MM',
+            'F' => 'Month',
+            'D' => 'Dy',
+            'd' => 'DD',
+            'l' => 'Day',
+            // No format for Day of the month without leading zeros.
+            'j' => 'DD',
+            'W' => 'IW',
+            'H' => 'HH24',
+            'h' => 'HH12',
+            'i' => 'MI',
+            's' => 'SS',
+            'A' => 'AM',
+        ],
+        'sqlsrv' => [
+            'A' => 'tt',
+            'd' => 'dd',
+            'F' => 'MMM',
+            'h' => 'hh',
+            'H' => 'HH',
+            'i' => 'mm',
+            'm' => 'MM',
+            // No format for 3 letter month name.
+            'M' => 'MMM',
+            's' => 'ss',
+            'Y' => 'yyyy',
+        ],
+    ];
+
+    /**
+     * @var array<'mysql'|'sqlite'|'pgsql'|'sqlsrv', array<string, string>>
+     */
+    protected array $emulatableCharacters = [
+        'mysql' => [
+            'U' => 'unix_timestamp(%s)',
+        ],
+        'sqlite' => [
+            'y' => 'substr(strftime(\'%Y\', %s), 3, 2)',
+            'U' => 'strftime(\'%%s\', %s)',
+        ],
+        'pgsql' => [
+            'U' => 'extract(epoch from %s)',
+        ],
+        'sqlsrv' => [
+            'n' => 'cast(month(%s) as varchar(2))',
+            'U' => 'datediff(second, \'1970-01-01\', %s)',
+        ],
+    ];
 
     public function __construct(
         private readonly string|Expression $expression,
@@ -22,111 +126,53 @@ class DateFormat implements Expression
 
     public function getValue(Grammar $grammar): string
     {
-        $expression = $this->stringize($grammar, $this->expression);
-        $format = $this->getDateFormat($grammar, $this->format);
+        /** @var non-empty-array<int, Expression> $expressions */
+        $expressions = [];
 
-        return match ($this->identify($grammar)) {
-            'mysql' => "date_format({$expression}, '{$format}')",
-            'sqlite' => "strftime('{$format}', {$expression})",
-            'pgsql' => "to_char({$expression}, '{$format}')",
-            'sqlsrv' => "format({$expression}, '{$format}')",
-        };
-    }
+        $lastFormatExpression = '';
 
-    protected function getDateFormat(Grammar $grammar, string $format): string
-    {
-        $expression = '';
+        foreach (str_split($this->format) as $index => $character) {
+            $emulatableCharacter = $this->emulatableCharacters[$this->identify($grammar)][$character] ?? null;
+            $formatCharacter = $this->formatCharacters[$this->identify($grammar)][$character] ?? null;
+            $styleCharacter = ! $emulatableCharacter && ! $formatCharacter ? $character : null;
 
-        foreach (str_split($format) as $char) {
-            $expression .= $this->getDateFormatChar($grammar, $char);
+            if ($styleCharacter || $formatCharacter) {
+                $lastFormatExpression .= $formatCharacter ?? $styleCharacter;
+            }
+
+            if (
+                $lastFormatExpression !== '' && (
+                    $emulatableCharacter || $index === strlen($this->format) - 1
+                )) {
+                $expressions[] = $this->getDateFormatExpression(
+                    $grammar,
+                    (string) $this->stringize($grammar, $this->expression),
+                    $lastFormatExpression,
+                );
+            }
+
+            if ($emulatableCharacter) {
+                $expressions[] = new QueryExpression(sprintf(
+                    $emulatableCharacter,
+                    $this->stringize($grammar, $this->expression)
+                ));
+            }
         }
 
-        return $expression;
+        if (count($expressions) == 1) {
+            return (string) $expressions[0]->getValue($grammar);
+        }
+
+        return (new Concat($expressions))->getValue($grammar);
     }
 
-    protected function getDateFormatChar(Grammar $grammar, string $char): string
+    protected function getDateFormatExpression(Grammar $grammar, string $expression, string $format): Expression
     {
         return match ($this->identify($grammar)) {
-            'mysql' => match ($char) {
-                'Y' => '%Y',
-                'y' => '%y',
-                'M' => '%b',
-                'm' => '%m',
-                'n' => '%c',
-                'F' => '%M',
-                'D' => '%a',
-                'd' => '%d',
-                'l' => '%W',
-                'j' => '%e',
-                'W' => '%v',
-                'H' => '%H',
-                'h' => '%h',
-                'i' => '%i',
-                's' => '%s',
-                'A' => '%p',
-                default => $char,
-            },
-            'sqlite' => match ($char) {
-                'Y' => '%Y',
-                // No format for 2 digit year number.
-                'y' => '%Y',
-                // No format for 3 letter month name.
-                'M' => '%m',
-                'm' => '%m',
-                // No format for month number without leading zeros.
-                'n' => '%m',
-                // No format for full month name.
-                'F' => '%m',
-                // No format for 3 letter day name.
-                'D' => '%d',
-                'd' => '%d',
-                // No format for full day name.
-                'l' => '%d',
-                // no format for day of month number without leading zeros.
-                'j' => '%d',
-                'W' => '%W',
-                'H' => '%H',
-                // No format for 12 hour with leading zeros.
-                'h' => '%H',
-                'i' => '%M',
-                's' => '%S',
-                default => $char,
-            },
-            'pgsql' => match ($char) {
-                'Y' => 'YYYY',
-                'y' => 'YY',
-                'M' => 'Mon',
-                'm' => 'MM',
-                // No format for Numeric representation of a month, without leading zeros.
-                'n' => 'MM',
-                'F' => 'Month',
-                'D' => 'Dy',
-                'd' => 'DD',
-                'l' => 'Day',
-                // No format for Day of the month without leading zeros.
-                'j' => 'DD',
-                'W' => 'IW',
-                'H' => 'HH24',
-                'h' => 'HH12',
-                'i' => 'MI',
-                's' => 'SS',
-                'A' => 'AM',
-                default => $char,
-            },
-            'sqlsrv' => match ($char) {
-                'A' => 'tt',
-                'd' => 'dd',
-                'F' => 'MMM',
-                'h' => 'hh',
-                'H' => 'HH',
-                'i' => 'mm',
-                'm' => 'MM',
-                // No format for 3 letter month name.
-                'M' => 'MMM',
-                's' => 'ss',
-                'Y' => 'yyyy',
-                default => $char,
-            },
+            'mysql' => new QueryExpression("date_format({$expression}, '{$format}')"),
+            'sqlite' => new QueryExpression("strftime('{$format}', {$expression})"),
+            'pgsql' => new QueryExpression("to_char({$expression}, '{$format}')"),
+            'sqlsrv' => new QueryExpression("format({$expression}, '{$format}')"),
         };
     }
 }
--- tests/Function/Date/DateFormatTest.php
+++ tests/Function/Date/DateFormatTest.php
@@ -5,7 +5,37 @@ declare(strict_types=1);
 use Illuminate\Database\Schema\Blueprint;
 use Tpetry\QueryExpressions\Function\Date\DateFormat;
 
-it('can format the date of a column to a Y-m')
+it('can format dates and don\'t use concat if not needed [Y]')
+    ->expect(new DateFormat('created_at', format: 'Y'))
+    ->toBeExecutable(callback: function (Blueprint $table) {
+        $table->dateTime('created_at');
+    })
+    ->toBePgsql('to_char("created_at", \'YYYY\')')
+    ->toBeSqlite('strftime(\'%Y\', "created_at")')
+    ->toBeMysql('date_format(`created_at`, \'%Y\')')
+    ->toBeSqlsrv('format([created_at], \'yyyy\')');
+
+it('can format dates and don\'t use concat if not needed: [U]')
+    ->expect(new DateFormat('created_at', format: 'U'))
+    ->toBeExecutable(callback: function (Blueprint $table) {
+        $table->dateTime('created_at');
+    })
+    ->toBePgsql('extract(epoch from "created_at")')
+    ->toBeSqlite('strftime(\'%s\', "created_at")')
+    ->toBeMysql('unix_timestamp(`created_at`)')
+    ->toBeSqlsrv('datediff(second, \'1970-01-01\', [created_at])');
+
+it('can format dates: [Y-n]')
+    ->expect(new DateFormat('created_at', format: 'Y-n'))
+    ->toBeExecutable(callback: function (Blueprint $table) {
+        $table->dateTime('created_at');
+    })
+    ->toBePgsql('to_char("created_at", \'YYYY-FMMM\')')
+    ->toBeSqlite('strftime(\'%Y-%m\', "created_at")')
+    ->toBeMysql('date_format(`created_at`, \'%Y-%c\')')
+    ->toBeSqlsrv('(concat(format([created_at], \'yyyy-\'),cast(month([created_at]) as varchar(2))))');
+
+it('can format dates: [Y-m]')
     ->expect(new DateFormat('created_at', format: 'Y-m'))
     ->toBeExecutable(callback: function (Blueprint $table) {
         $table->dateTime('created_at');
@@ -15,7 +45,7 @@ it('can format the date of a column to a Y-m')
     ->toBeMysql('date_format(`created_at`, \'%Y-%m\')')
     ->toBeSqlsrv('format([created_at], \'yyyy-MM\')');
 
-it('can format the date of a column to a Y-m-d H:i:s')
+it('can format dates: [Y-m-d H:i:s]')
     ->expect(new DateFormat('created_at', format: 'Y-m-d H:i:s'))
     ->toBeExecutable(callback: function (Blueprint $table) {
         $table->dateTime('created_at');
